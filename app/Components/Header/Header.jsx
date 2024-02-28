2d5fa584f13f0212db8547f98fe8ad1bcf6d33f9
Ray <buttrafay980@gmail.com>
2024-02-28 16:23:08 +0500

    updatg

--- app/Components/Header/Header.jsx
+++ app/Components/Header/Header.jsx
@@ -2,45 +2,72 @@ import { memo, useEffect, useState } from 'react';
 import styles from "./Header.module.css";
 import { useData } from '@/app/Contexts/DataContext/DataContext';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const Header = ( { className, openPopup } ) => {
 
   const { data, dataloading, currentTeam } = useData();
   const [ createTeamPopupOpen, setCreateTeamPopupOpen ] = useState( false );
+  const [ dropDownOpen, toggleDropDown ] = useState( false );
   const router = useRouter();
+
+  const variants = {
+    open: {
+      scale: 1
+    },
+    close: {
+      scale: 0
+    }
+  };
 
   useEffect( () => {
     console.log( "teams: ", data?.sessionData?.currentUserData.current_user_teams_data );
     console.log( "currentTeam: ", currentTeam );
   }, [ data ] );
 
-  if ( dataloading ) {
+  if ( dataloading && !data ) {
     return <header className={ `${ className && className }` }>
       Loading...
     </header>;
   }
 
+  const ToggleDropDown = () => toggleDropDown( prev => !prev );
+  const closeDropDown = () => toggleDropDown( false );
+
   return (
     <header id={ styles[ "header" ] } className={ className }>
       <div className={ styles[ "teams-search" ] }>
-        <div className={ styles[ "teams" ] }>
+        <div className={ styles[ "teams" ] } onClick={ ToggleDropDown }>
 
-          <select name="teamNames" id="teamNames" onChange={ ( e ) => {
-            const option = e.target.selectedOptions[ 0 ].value;
-            if ( option == "/teams/create" ) {
-              openPopup();
-              console.log( "opneeened" );
-            } else {
-              router.push( option );
-            }
-          } } >
+          <div className={ styles[ 'select' ] } name="teamNames" id="teamNames" >
+            <p className={ styles[ "default-team" ] }>{ currentTeam?.teamName }</p>
 
-            { data?.sessionData?.currentUserData.current_user_teams_data.map( ( team, i ) => (
-              <option value={ `/teams/${ team.id }` } key={ i } selected={ currentTeam?.teamID == team.id }>{ team.teamName }</option>
-            ) ) }
+          </div>
+          <AnimatePresence mode='wait'>
+            { dropDownOpen && (
+              <motion.div className={ styles[ "other-teams" ] } variants={ variants } animate="open" initial="close" exit={ "close" }>
+                { data?.sessionData?.currentUserData.current_user_teams_data.map( ( team, i ) => (
+                  <>
+                    { team.id != currentTeam?.teamID && (
+                      <Link key={ i } href={ `/teams/${ team.id }` } onClick={ ( e ) => {
+                        closeDropDown();
+                        // router.push( `/teams/${ team.id }` );
+                      } }>{ team.teamName }</Link>
+                    ) }
+                  </>
+                ) ) }
 
-            <option value={ "/teams/create" } key={ "create" }>Create New Team</option>
-          </select>
+                <Link href={ "/teams/create" } key={ "create" } onClick={ ( e ) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  closeDropDown();
+                  openPopup();
+                  console.log( "opneeened" );
+                } }>Create New Team</Link>
+              </motion.div>
+            ) }
+          </AnimatePresence>
         </div>
       </div>
     </header>
