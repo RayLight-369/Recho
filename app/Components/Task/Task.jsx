36868f77ea930af77da6df22caa176225b15c526
Ray <buttrafay980@gmail.com>
2024-02-28 21:42:30 +0500

    upgrade

--- app/Components/Task/Task.jsx
+++ app/Components/Task/Task.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Styles from "./Task.module.css";
 import { Reorder, motion } from 'framer-motion';
+import { PRIORITY, STATUS } from '@/app/utils/Constants';
 
 const Task = ( { task } ) => {
 
   return (
     <Reorder.Item value={ task } as='div' className={ Styles[ "task" ] }
+      style={ {
+        borderLeft: `4px solid ${ STATUS( task.status )[ 1 ] }`
+      } }
       id={ task.id }
       initial={ { opacity: 0, y: 30 } }
       animate={ {
@@ -14,8 +18,11 @@ const Task = ( { task } ) => {
         transition: { duration: 0.15 }
       } }
       exit={ { opacity: 0, y: 20, transition: { duration: 0.3 } } }
-      whileDrag={ { backgroundColor: "#e3e3e3" } }>
-      <motion.p layout="position" className={ Styles[ "title" ] }>{ task?.title.length > 50 ? task?.title.slice( 0, 37 ) + "..." : task?.title }</motion.p>
+      whileDrag={ { scale: 1.04 } }>
+
+      <motion.p layout="position" className={ Styles[ "title" ] } >
+        { task?.title.length > 50 ? task?.title.slice( 0, 37 ) + "..." : task?.title }
+      </motion.p>
       <motion.p layout className={ Styles[ "reporter" ] }>{ "Ray" }</motion.p>
       <motion.p layout className={ Styles[ "date" ] }>{ "27-feb 2024" }</motion.p>
       <motion.p layout className={ Styles[ "priority" ] }>{ "Medium" }</motion.p>
--- app/utils/Constants.js
+++ app/utils/Constants.js
@@ -1,7 +1,12 @@
 export const STATUS = ( value ) => [
-  "progress",
-  "approval",
-  "complete"
+  [ "progress", "#0035ff" ],
+  [ "approval", "#ffaa00" ],
+  [ "complete", "#00dd88" ],
 ][ value ];
 
-export const PRIORITY = ( value ) => [ "neutral", "low", "medium", "high" ][ value ];
+export const PRIORITY = ( value ) => [
+  [ "neutral", "#343443" ],
+  [ "low", "#00ddcc" ],
+  [ "medium", "#ffaa00" ],
+  [ "high", "#ff0044" ]
+][ value ];
